c0eb87aa36351ba5002c609c0a9e2ad5b4439d63
Gregory DEPUILLE <gregory.depuille@gmail.com>
2024-04-26 13:18:32 +0200

    Manage all the other robot from one screen.

--- roles/arig_robot/templates/home/pi/robot-launcher.sh
+++ roles/arig_robot/templates/home/pi/robot-launcher.sh
@@ -9,11 +9,16 @@ fi
 ACTION="init"
 EXTERNAL_DIR="/tmp/external-dir"
 ROBOT_NAME=$(cat robot-name)
-if [ "${ROBOT_NAME}" = "nerell" ] ; then
-    OTHER_ROBOT_NAME="odin"
-else
-    OTHER_ROBOT_NAME="nerell"
-fi
+OTHER_ROBOTS_NAME="nerell odin pami-triangle pami-carre pami-rond"
+
+function send_action_to_other_robots() {
+    ACTION=${1}
+    for OTHER_ROBOT_NAME in ${OTHER_ROBOTS_NAME} ; do
+        if [ "${OTHER_ROBOT_NAME}" != "${ROBOT_NAME}" ] ; then
+            ssh ${OTHER_ROBOT_NAME} touch ${EXTERNAL_DIR}/${ACTION}
+        fi
+    done
+}
 
 while [ "${ACTION}" != "exit" ] ; do
     cd
@@ -25,16 +30,19 @@ while [ "${ACTION}" != "exit" ] ; do
     echo "Action : ${ACTION}"
 
     if [ "${ACTION}" == "run" ] ; then
-        ssh ${OTHER_ROBOT_NAME} touch ${EXTERNAL_DIR}/run
+        send_action_to_other_robots "${ACTION}"
         ./run.sh
     elif [ "${ACTION}" == "monitoring" ] ; then
-        ssh ${OTHER_ROBOT_NAME} touch ${OTHER_ROBOT_NAME}:${EXTERNAL_DIR}/monitoring
+        send_action_to_other_robots "${ACTION}"
         ./monitoring.sh
     elif [ "${ACTION}" == "debug" ] ; then
+        send_action_to_other_robots "${ACTION}"
         ./debug.sh
     elif [ "${ACTION}" == "reboot" ] ; then
+        send_action_to_other_robots "${ACTION}"
         sudo reboot
     elif [ "${ACTION}" == "poweroff" ] ; then
+        send_action_to_other_robots "${ACTION}"
         sudo poweroff
     fi
 
